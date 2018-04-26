5c28bfcc6e8afeac32ece9777f1dfc8b2d692131
savillK <38045454+savillK@users.noreply.github.com>
2018-04-26 16:05:17 -0400

    Update index.js

--- Website/index.js
+++ Website/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     $('#resume_page').hide();
     $('#other_interests').hide();
-	document.getElementById('resume_link').onclick = function() {
-		$('#resume_page').show();
-		$('#main_page_content').hide();
+    document.getElementById('resume_link').onclick = function() {
+	$('#resume_page').show();
+	$('#main_page_content').hide();
         $('#other_interests').hide();
-	};
-	document.getElementById('Other').onclick = function() {
+    };
+    document.getElementById('Other').onclick = function() {
         $('#resume_page').hide();
         $('#main_page_content').hide();
-		$('#other_interests').show();
-	};
+	$('#other_interests').show();
+    };
     document.getElementById('about_lan').onclick = function() {
         $('#resume_page').hide();
         $('#main_page_content').show();
