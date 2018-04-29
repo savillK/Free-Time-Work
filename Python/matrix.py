ed13384301704c890a9d30b79949fb58d7e74270
savillK <38045454+savillK@users.noreply.github.com>
2018-04-29 13:56:28 -0400

    Update matrix.py

--- Python/matrix.py
+++ Python/matrix.py
@@ -5,27 +5,18 @@ b = [3.0, 4.0, 5.0]
 
 def gauss_elim(A, b):
     n = len(b)
-    #i = 0
     for i in range(0,n-1):
-    #while i < n-1:
         #If diagonal is zero
     	if A[i][i] == 0:
     		print("error")
     		return (None, None)
-        #j = i+1
         #iterate through each row
         for j in range(i+1, n):
-       #while j < n:
-            #k = i
             b[j] = b[j] - (A[j][i] / A[i][i]) * b[i]
             t = (A[j][i] / A[i][i]) #Fraction by which each row is reduced
             #iterate through each column
             for k in range(i, n):
-            #while k < n:
                 A[j][k] = A[j][k] - ((t) * A[i][k])
-                #k += 1
-            #j += 1
-        #i += 1
     return (A, b)
 
 def back_sub(A, b):
